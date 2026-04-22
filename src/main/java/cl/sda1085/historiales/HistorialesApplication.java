72856321081c6f345356368c5006e2d63d8a78e2
Gonza-2103 <ghormazabal.d@gmail.com>
2026-04-22 14:14:41 -0400

    Initial commit

---
+++ src/main/java/cl/sda1085/historiales/HistorialesApplication.java
@@ -0,0 +1,13 @@
+package cl.sda1085.historiales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistorialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistorialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sda1085/historiales/HistorialesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sda1085.historiales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
